a852241f9ed62a56c439615f0801cd160c5cdcb5
Esteban Pinelli <81484588+estebanpinelli@users.noreply.github.com>
2023-05-19 17:59:19 +0000

    iniciado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	
+	useEffect (() => {
+
+	fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => {
+        console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+        console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+        return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+    })
+    .then(data => {
+        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+    })
+    .catch(error => {
+        //manejo de errores
+        console.log(error);
+    });
+});
+
+
+  const [data, setData] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  const handleChange = (event) => {
+    setData(event.target.value);
+  };
+
+  const addTodo = () => {
+    setTodo([...todo, { data }]);
+
+  };
+
+  return (
+    <>
+      <input type="text" name="text" value={data} onChange={handleChange} />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todo.map((todoItem, index) => {
+          return <li key={index}>{todoItem.data}</li>;
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default Home;
